63657e967934084d99161fb68a50b3395b7f55ce
Brunno Costa <sbrunno.costa@gmail.com>
2022-09-16 18:41:48 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/ads', (request: any, response: any) => {
+
+    return response.json([
+        { id: '1', name: 'Anúncio 1'},
+        { id: '2', name: 'Anúncio 2'},
+        { id: '3', name: 'Anúncio 3'},
+        { id: '4', name: 'Anúncio 4'}
+    ])
+})
+
+app.listen(3333)
